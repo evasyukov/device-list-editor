5ff179ba4f3a69262a1f11e134aab375886b059c
evasyukov <egor.vasyukov1@gmail.com>
2024-10-24 14:02:12 +0700

    добавил функционал редактирования устройства

--- src/components/Device.vue
+++ src/components/Device.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="device">
     <div v-if="editing">
+        
+      <input v-model="deviceName" placeholder="Название устройства" />
+      <div class="nodes">
+        <h4>Узлы устройства</h4>
+        <ul>
+          <li v-for="node in device.nodes" :key="node.id">
+            {{ node.name }}
+          </li>
+        </ul>
+        <button @click="addNode">Добавить узел</button>
+      </div>
 
+      <button @click="saveEdit">Сохранить</button>
     </div>
 
     <div v-else>
@@ -11,7 +23,7 @@
           {{ node.name }}
         </li>
       </ul>
-      <button>Редактировать</button>
+      <button @click="toggleEdit">Редактировать</button>
       <button>Удалить</button>
     </div>
   </div>
@@ -19,10 +31,25 @@
 
 <script setup>
 import { ref } from "vue"
+import { useDeviceStore } from '../store/devices'
+
+import Node from "./Node.vue"
 
 const props = defineProps(["device"])
 const editing = ref(false)
+const deviceName = ref(props.device.name)
+const deviceStore = useDeviceStore()
 
+// переключение режима редактирования
+const toggleEdit = () => {
+  editing.value = !editing.value
+}
+
+// сохранение изменений устройства
+const saveEdit = () => {
+  deviceStore.editDevice(props.device.id, deviceName.value)
+  editing.value = false
+}
 </script>
 
 <style scoped>
--- src/store/devices.js
+++ src/store/devices.js
@@ -3,19 +3,28 @@ import { reactive } from "vue"
 
 export const useDeviceStore = defineStore("deviceStore", () => {
   const devices = reactive([
-    { id: 1, name: "Устройство 1", nodes: [{ id: 1, name: "Узел 1" }] },
-    { id: 2, name: "Устройство 2", nodes: [] },
+    { id: 1, name: "Телефон", nodes: [{ id: 1, name: "Нокиа" }] },
+    { id: 2, name: "Пылесос", nodes: [] },
     {
       id: 3,
-      name: "Устройство 2",
+      name: "Ноутбук",
       nodes: [
-        { id: 1, name: "Узел 1" },
-        { id: 2, name: "Узел 2" },
+        { id: 1, name: "Леново" },
+        { id: 2, name: "Самсунг" },
       ],
     },
   ])
 
+  // редактирование узла устройства
+  const editDevice = (id, newName) => {
+    const device = devices.find((device) => device.id === id)
+    if (device) {
+      device.name = newName
+    }
+  }
+
   return {
     devices,
+    editDevice,
   }
 })
